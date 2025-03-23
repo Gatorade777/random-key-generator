b6565fda9db558880f7957cfe20b44beb6d6ec8b
Gatorade777 <andrsarias9@gmail.com>
2025-03-23 01:48:56 -0500

    grammar correction of comments and prints

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,12 @@
-#well iam newbi so this is mi first proyect, if a make some mistakes i accept correptions also i dont speak inglish
+#Password generator by Gatorade777
 
 
-#code idea---->
+#code idea----> A program that allows the user to create a secure password that can be stored locally and used when login is required.
 
-#def the function to generate the random password
+
 import random as rm #for random characters
 import string as st #for all the characters in the paswword
+#def the function to generate the random password
 
 def random_key_generator(length,numbers,upper,special):
     characters = st.ascii_lowercase #the password by default has lower cases
@@ -18,11 +19,13 @@ def random_key_generator(length,numbers,upper,special):
     if not characters:
         raise ValueError
 
-    password = "".join(rm.choice(characters) for _ in range(length)) #add the charcacters that you choose in a random place with the length you choose(i dont know if that is a corret grammar)
+    password = "".join(rm.choice(characters) for _ in range(length))#Adds the character(s) of your choice at a random location with a length of your choice.
     return password
 
-#ask the length of the password, ask if want lower and upper cases, also ask if want special charactes,
-def main():#maybe this function has to be ahead of the other XD
+#Ask for the length of the password, ask if they want lowercase and uppercase letters, and ask if they want special characters
+
+
+def main():
     attempts = 0
     attempts2 = 0
     #bucle for attemps in the length
@@ -30,31 +33,32 @@ def main():#maybe this function has to be ahead of the other XD
         
         try:
             
-            length = int(input("enter(in number) the length of the password that you want: "))
+            length = int(input("Enter the desired password length (in numbers). The minimum recommended for a strong password is 8:  "))
             if length <= 0:
                 if attempts2 == 2:
                     print("you're really stupid bro xd")
                     break
                 attempts2 += 1
-                print("only positive numbers ")
+                print("Non-zero positive numbers only ")
             else:
                 break
-        except ValueError:#because the other valueError
+        except ValueError:
             
             if attempts == 2:
                 print("you're really stupid bro xd")
                 break
             attempts  += 1
-            print("pls enter a valid NUMBER for the length of your password")#this is in case the user dont put a number in the lenght
+            print("Enter a valid NUMBER for your password length.")#This is in case the user does not enter a valid number in length.
     
         
                 
     if attempts == 2 or attempts2 == 2:
         return print("sorry we cant create your password, try again")
     else:
-        numbers = input("you want use numbers in the password? (y/n)").lower() == "y"
-        upper = input("you want use upper cases in the password? (y/n)").lower() == "y" #in this case if the user respond y o Y upper become True if respond somenthing else is false
-        special = input("you want to use special characters for you password? (y/n)").lower() == "y"
+        #in this case if the user respond y o Y upper become True if respond somenthing else is false
+        numbers = input("Do you want to use numbers in the password? (y/n):").lower() == "y"
+        upper = input("Do you want to use uppercase letters in the password? (y/n):").lower() == "y" 
+        special = input("Do you want to use special characters in your password? (y/n):").lower() == "y"
     
         password = random_key_generator(length, numbers, upper, special)
         print(f"Here is your ultra professional secret password: {password}")
